472df57e92fd86cf8a4a34e472d09317c943dc24
thirumoorthy2005 <79498283+thirumoorthy2005@users.noreply.github.com>
2022-10-19 11:39:55 +0530

    firstcommit

---
+++ src/main/java/com/capeelectic/CapeLmsPhdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capeelectic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapeLmsPhdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapeLmsPhdBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capeelectic/CapeLmsPhdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capeelectic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapeLmsPhdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
